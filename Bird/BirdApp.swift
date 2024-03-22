78f8eb1468944e9ddc33e7590bad18b83fae8753
Llama <fabsese123@icloud.com>
2024-03-22 04:55:25 -0400

    Initial Commit

---
+++ Bird/BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdApp.swift
+//  Bird
+//
+//  Created by Llamalaptop on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
